555c170dca36af244e93324bb38108310b4812d1
bastiaaan <b.m.mevius@student.tue.nl>
2020-03-10 12:41:04 +0100

    Fixed some bugs

--- coverage_calculator.py
+++ coverage_calculator.py
@@ -1,4 +1,5 @@
 import json
+import os
 import sys
 from os import listdir
 from os.path import isfile, join
@@ -7,17 +8,20 @@ import matplotlib.pyplot
 
 # order of arguments:
 #   version1 version2 loc1 loc2
-path = "/usr/jquery-data/jsinpect"
+path = "/usr/jquery-data/jsinspect"
 onlyfiles = [f for f in listdir(path) if isfile(join(path, f))]
 
 for file in onlyfiles:
-    version1 = file.split(".")[0].split("-")[0]
-    version2 = file.split(".")[0].split("-")[1]
+    version1 = file.split(".json")[0].split("-")[0]
+    version2 = file.split(".json")[0].split("-")[1]
+    if version1 == "jsinspect" or version1 == "cloc" or version2 == "jsinspect" or version2 == "cloc":
+        continue
     loc1_file = json.load(open(f"/usr/jquery-data/cloc/{version1}.json"))
     loc1 = loc1_file["JavaScript"]["blank"] + loc1_file["JavaScript"]["comment"] + loc1_file["JavaScript"]["code"]
     loc2_file = json.load(open(f"/usr/jquery-data/cloc/{version2}.json"))
     loc2 = loc2_file["JavaScript"]["blank"] + loc2_file["JavaScript"]["comment"] + loc2_file["JavaScript"]["code"]
-    code_dup_json = json.load(open("test.json"))
+    print(f"/usr/jquery-data/jsinspect/{file}")
+    code_dup_json = json.load(open(f"/usr/jquery-data/jsinspect/{file}"))
     coverage = 0
     for match in code_dup_json:
         version1_min = 0
--- static_commands.py
+++ static_commands.py
@@ -3,16 +3,16 @@ import os
 subdirectories = sorted(next(os.walk("/usr/jquery-data"))[1])
 i = 0
 for directory1 in subdirectories:
-    cloc_command = f"cloc {directory1}/src --json > /usr/jquery-data/cloc/{directory1}.json"
+    cloc_command = f"cloc {directory1}/src --json --out=/usr/jquery-data/cloc/{directory1}.json"
     os.system(cloc_command)
-    os.system(f"echo {cloc_command}")
+    print(f"{cloc_command}")
     for directory2 in subdirectories:
         if directory1 == directory2:
             os.system("echo Break")
             break
-        jsinspect_command = f"jsinspect {directory1}/src {directory2}/src -I -L --truncate 1 -r json > " \
-            f"/usr/jquery-data/jsinspect/{directory1}-{directory2}.json"
+        jsinspect_command = f"jsinspect {directory2}/src {directory1}/src -I -L --truncate 1 -r json > " \
+            f"/usr/jquery-data/jsinspect/{directory2}-{directory1}.json"
         os.system(jsinspect_command)
-        os.system(f"echo {jsinspect_command}")
-        os.system(f"echo Voortgang {i}")
+        print(f"{jsinspect_command}")
+        print(f"Voortgang {i}")
         i += 1
